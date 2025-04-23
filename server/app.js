9fdcc2a1abb1d118876892e8a2c7495552619bf7
JacobArndt <jacobwarndt@gmail.com>
2025-04-23 14:02:23 -0400

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
-// import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json');
+const app = express();
+const PORT = 3000;
 
-
-// create your express server below
-var app;
-
-// add your routes and middleware below
-
-// finally export the express application
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+    res.status(200).send("It's working!");
+});
+app.get('/data', (req, res) => {
+    res.status(200).json(data);
+});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}
 module.exports = app;
